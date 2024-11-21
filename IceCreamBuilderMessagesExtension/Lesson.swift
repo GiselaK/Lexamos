b140afe4c8b4ccf9e0c139539afb131c019c6f29
giselak <gisela.kottmeier@edovo.com>
2024-11-21 03:15:57 -0800

    Lessons View Added

---
+++ IceCreamBuilderMessagesExtension/Lesson.swift
@@ -0,0 +1,16 @@
+//
+//  Game.swift
+//  Lexamos
+//
+//  Created by Gisela K on 11/20/24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Lesson {
+    let sticker: UIImage
+    let name: String
+    let comingSoon: Bool
+}
--- IceCreamBuilderMessagesExtension/LessonCell.swift
+++ IceCreamBuilderMessagesExtension/LessonCell.swift
@@ -1,3 +1,12 @@
+//
+//  LessonCell.swift
+//  Lexamos
+//
+//  Created by Gisela K on 11/21/24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import Foundation
 /*
 See the LICENSE.txt file for this sample’s licensing information.
 
@@ -8,15 +17,16 @@ A `UICollectionViewCell` subclass used to display an `IceCream` in the `IceCream
 import UIKit
 import Messages
 
-class IceCreamCell: UICollectionViewCell {
+class LessonCell: UICollectionViewCell {
     
-    static let reuseIdentifier = "IceCreamCell"
+    static let reuseIdentifier = "LessonCell"
     
-    var representedIceCream: IceCream?
-    var selectGame: (() -> Void)?
+//    var representedIceCream: IceCream?
+    var selectLesson: (() -> Void)?
     @IBOutlet weak var sticker: UIImageView!
     @IBOutlet weak var name: UILabel!
     @IBAction func buttonTapped(_ sender: UIButton) {
-        selectGame?()
+        selectLesson?()
     }
 }
+
